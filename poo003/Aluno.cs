450e8b974ce5359a34d051efc0a768f0f5f6b0b2
Felipe Costa <felipecostasiqu@gmail.com>
2025-01-21 16:03:53 -0300

    Exercício 003

---
+++ poo003/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace poo003
+{
+    internal class Aluno
+    {
+        // Atributos
+        public string nome;
+        public double nota1, nota2;
+
+        // Métodos
+        public double Media()
+        {
+            return (nota1 + nota2) / 2;
+        }
+
+        public string Situação(double media)
+        {
+            return media >= 5 ? "Aprovado" : "Reprovado";
+        }
+
+        public void Mensagem()
+        {
+            Console.WriteLine($"{nome} sua média é {Media()} você foi {Situação(Media())}");
+        }
+    
+    }
+
+}
---
+++ poo003/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace poo003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno camilaAluno = new Aluno();
+            camilaAluno.nome = "Camila";
+            camilaAluno.nota1 = 7;
+            camilaAluno.nota2 = 10;
+            camilaAluno.Mensagem();
+        }
+
+    }
+}
