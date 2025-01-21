02dbef5d337dfdef5519d77cf6bb263a5cfbde4a
Felipe Costa <felipecostasiqu@gmail.com>
2025-01-21 16:23:29 -0300

    Exercício 004

---
+++ poo004/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace poo004
+{
+    internal class Pessoa
+    {
+        public double peso;
+        public double altura;
+
+        public double CalculoImc()
+        {
+            double imc = peso / (altura * altura);
+            return Math.Round(imc, 2);
+            
+        }
+
+        public void Mensagem()
+        {
+            if (CalculoImc() <= 18.5)
+            {
+                Console.WriteLine($"Você está com IMC {CalculoImc()}. Você esta abaixo do peso");
+            }
+            else if (CalculoImc() >= 18.6 && CalculoImc() <= 25)
+            {
+                Console.WriteLine($"Você está com IMC {CalculoImc()}. Você esta com peso normal");
+            }
+            else if (CalculoImc() >= 26 && CalculoImc() <= 30)
+            {
+                Console.WriteLine($"Você está com IMC {CalculoImc()}. Você acima do peso");
+            }
+            else if (CalculoImc() >= 31 && CalculoImc() <= 35)
+            {
+                Console.WriteLine($"Você está com IMC {CalculoImc()}. Você com obesidade 1");
+            }
+            else if (CalculoImc() >= 36 && CalculoImc() <= 40)
+            {
+                Console.WriteLine($"Você está com IMC {CalculoImc()}. Você com obesidade 2");
+            }
+            else
+            {
+                Console.WriteLine($"Você está com IMC {CalculoImc()}. Você com obesidade 3");
+            }
+        }
+
+    }
+}
---
+++ poo004/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace poo004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa teste01 = new Pessoa();
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Qual sua altura?: ");
+                    teste01.altura = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("[ERROR] Tente novamente!");
+                }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Qual sua peso?: ");
+                    teste01.peso = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("[ERROR] Tente novamente!");
+                }
+            }
+            teste01.Mensagem();
+        }
+    }
+}
