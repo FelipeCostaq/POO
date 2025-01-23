851ccb17e2a42c5294a8e9b8c116be486f1c0bf7
Felipe Costa <felipecostasiqu@gmail.com>
2025-01-23 11:35:02 -0300

    Exercício 005

---
+++ poo005/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace poo005
+{
+    internal class Aluno
+    {
+        // Atributos
+        private double nota1, nota2;
+
+        // Média
+        private double Media()
+        {
+            return (nota1 + nota2) / 2;
+        }
+
+        // Mensagem
+        public void Mensagem()
+        {
+            Console.Write("Nota 1: ");
+            nota1 = int.Parse(Console.ReadLine());
+            Console.Write("Nota 2: ");
+            nota2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A média entre {nota1} e {nota2} é {Media()}");
+        }
+    }
+}
---
+++ poo005/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace poo005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno01 = new Aluno();
+            aluno01.Mensagem();
+        }
+    }
+}
