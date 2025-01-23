604fdf2238abac45e056f584845c4973af052024
Felipe Costa <felipecostasiqu@gmail.com>
2025-01-23 18:41:16 -0300

    Exercício 006

---
+++ poo006/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace poo006
+{
+    internal class Pessoa
+    {
+        // Construtor
+        public Pessoa()
+        {
+            Console.WriteLine("Construtor Executado!");
+        }
+
+        // Construtor com Parâmetro
+        public Pessoa(string nome)
+        {
+            Console.WriteLine($"Olá, {nome}");
+        }
+
+    }
+}
---
+++ poo006/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace poo006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("Felipe");
+            
+        }
+    }
+}
